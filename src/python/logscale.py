8a74660db8b011d8c2dbd31860b8401a682f0c77
Shusei Matoike <smat1957@gmail.com>
2023-10-06 12:15:43 +0900

    update

--- src/python/logscale.py
+++ src/python/logscale.py
@@ -9,14 +9,24 @@ decade = 5
 for n in range(decade):
     for x in range(1*pow(10, n), 10*pow(10, n)+10*n, pow(10,n)):
         if x==pow(10,n) or x==10*pow(10, decade-1):
-            ax.plot( [x,x], [0,10], color="0.5", linewidth="0.8", linestyle="solid" )
+            clr = "0.5"
+            lnwdt = "0.8"
+            lnstyl = "solid"
         else:
-            ax.plot( [x,x], [0,10], color="0.2", linewidth="0.5", linestyle=(0, (1, 3)) )
+            clr = "0.2"
+            lnwdt = "0.5"
+            lnstyl = (0, (1, 3))
+        ax.plot( [x,x], [0,10], color=clr, linewidth=lnwdt, linestyle=lnstyl )
 for y in range(0, ymax + 1):
     if y==0 or y==ymax:
-        ax.plot( [1, pow(10, decade)], [y, y], color="0.2", linewidth="0.5", linestyle="solid" )
+        clr = "0.2"
+        lnwdt = "0.5"
+        lnstyl = "solid"
     else:
-        ax.plot( [1, pow(10, decade)], [y, y], color="0.2", linewidth="0.5", linestyle="dotted" )
+        clr = "0.2"
+        lnwdt = "0.5"
+        lnstyl = "dotted"
+    ax.plot( [1, pow(10, decade)], [y, y], color=clr, linewidth=lnwdt, linestyle=lnstyl )
 ax.set_xscale('log')
 
 plt.axis("off")
