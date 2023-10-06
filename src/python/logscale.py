e727ca8bea845dbde9d5c63af0fb84a7b5b42394
Shusei Matoike <smat1957@gmail.com>
2023-10-06 10:40:47 +0900

    update

--- src/python/logscale.py
+++ src/python/logscale.py
@@ -8,9 +8,15 @@ ymax = 10
 decade = 5
 for n in range(decade):
     for x in range(1*pow(10, n), 10*pow(10, n)+10*n, pow(10,n)):
-        ax.plot( [x,x], [0,10], color="0.5", linewidth="1" )
+        if x==pow(10,n) or x==10*pow(10, decade-1):
+            ax.plot( [x,x], [0,10], color="0.5", linewidth="0.8", linestyle="solid" )
+        else:
+            ax.plot( [x,x], [0,10], color="0.2", linewidth="0.5", linestyle=(0, (1, 3)) )
 for y in range(0, ymax + 1):
-    ax.plot( [1, pow(10, decade)], [y, y], color="0.5", linewidth="1" )
+    if y==0 or y==ymax:
+        ax.plot( [1, pow(10, decade)], [y, y], color="0.2", linewidth="0.5", linestyle="solid" )
+    else:
+        ax.plot( [1, pow(10, decade)], [y, y], color="0.2", linewidth="0.5", linestyle="dotted" )
 ax.set_xscale('log')
 
 plt.axis("off")
